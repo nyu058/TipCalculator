4043a125188a287b3e8ffa545729f01d6cfe190e
Rachel <jiarui025@gmail.com>
2018-05-24 11:54:11 -0400

    ~

---
+++ app/src/main/java/ca/uottawa/tipcalculator/SettingsActivity.java
@@ -0,0 +1,65 @@
+package ca.uottawa.tipcalculator;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.view.View.OnClickListener;
+
+public class SettingsActivity extends AppCompatActivity implements OnClickListener {
+    TextView defaultTipPercentageTextView;
+    TextView currencyTextView;
+    EditText defaultTipPercentageEditText;
+    TextView percentageSymbolTextView;
+    Spinner currencySpinner;
+    Button confirmButton;
+    Button resetButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+
+        defaultTipPercentageTextView = (TextView) findViewById(R.id.defaultTipPercentageTextView);
+        currencyTextView = (TextView) findViewById(R.id.currencyTextView);
+        defaultTipPercentageEditText = (EditText) findViewById(R.id.defaultTipPercentageEditText);
+        percentageSymbolTextView = (TextView) findViewById(R.id.defaultTipPercentageTextView);
+
+        //两个button没有分别做intent,问题可能出在这里
+        confirmButton.setOnClickListener(this);
+        resetButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(validateInput()){
+            //这里要存一下editText input的值
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    public boolean checkEmpty(EditText editText){
+        if(TextUtils.isEmpty(editText.getText().toString())){
+            editText.setError("This field cannot be blank!");
+            return true;
+        }
+        return false;
+    }
+
+    public boolean validateInput(){
+        if(checkEmpty(defaultTipPercentageEditText)){
+            return false;
+        }
+        return true;
+    }
+}
