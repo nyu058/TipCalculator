111823fdc7ea086c03abb0d917b1295c5d6d7eec
Rachel <jiarui025@gmail.com>
2018-06-07 11:55:48 -0400

    ~

--- app/src/main/java/ca/uottawa/tipcalculator/SettingsActivity.java
+++ app/src/main/java/ca/uottawa/tipcalculator/SettingsActivity.java
@@ -7,7 +7,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextUtils;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -23,6 +25,7 @@ public class SettingsActivity extends AppCompatActivity implements OnClickListen
     Spinner currencySpinner;
     Button confirmButton;
     Button resetButton;
+    Button cancelButton;
     SharedPreferences pref;
 
     @Override
@@ -33,21 +36,27 @@ public class SettingsActivity extends AppCompatActivity implements OnClickListen
         Intent intent = getIntent();
         Bundle bundle = intent.getExtras();
         currencySpinner= (Spinner) findViewById(R.id.currencySpinner);
+
         ArrayAdapter<CharSequence> currencyAdapter = ArrayAdapter.createFromResource(this,R.array.entries, R.layout.spinner);
         currencyAdapter.setDropDownViewResource(R.layout.spinner);
         currencySpinner.setAdapter(currencyAdapter);
+
         defaultTipPercentageTextView = (TextView) findViewById(R.id.defaultTipPercentageTextView);
         currencyTextView = (TextView) findViewById(R.id.currencyTextView);
         defaultTipPercentageEditText = (EditText) findViewById(R.id.defaultTipPercentageEditText);
 
         confirmButton = (Button) findViewById(R.id.confirmButton);
         resetButton = (Button) findViewById(R.id.resetButton);
+        cancelButton = (Button) findViewById(R.id.cancelButton);
 
         confirmButton.setOnClickListener(this);
         resetButton.setOnClickListener(this);
+        cancelButton.setOnClickListener(this);
+
         pref = getApplicationContext().getSharedPreferences("MyPref", Context.MODE_PRIVATE);
         defaultTipPercentageEditText.setText(pref.getString("percentage", "15"));
         currencySpinner.setSelection(setCurrencySpinner());
+
     }
 
     @Override
@@ -65,9 +74,15 @@ public class SettingsActivity extends AppCompatActivity implements OnClickListen
                 Intent intent = new Intent(this, MainActivity.class);
                 startActivity(intent);
             }
-        }//else if (view.getId() ==R.id.resetButton){
-            //todo
-       // }
+        }else if (view.getId() == R.id.resetButton) {
+
+            defaultTipPercentageEditText.setText("");
+            currencySpinner.setSelection(0);
+
+        }else if(view.getId() == R.id.cancelButton){
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
     }
 
     public boolean checkEmpty(EditText editText){
@@ -78,9 +93,23 @@ public class SettingsActivity extends AppCompatActivity implements OnClickListen
         return false;
     }
 
-    public boolean validateInput(){
+    public boolean checkValidity(EditText editText){
+        int amount = Integer.parseInt(editText.getText().toString());
+        if(amount <0 || amount >100){
+            editText.setError("Please enter an amount between 0 and 100");
+            return true;
+        }
+        return false;
+    }
 
-        return !checkEmpty(defaultTipPercentageEditText);
+    public boolean validateInput(){
+        if(checkEmpty(defaultTipPercentageEditText)){
+            return false;
+        }
+        else if(checkValidity(defaultTipPercentageEditText)){
+            return false;
+        }
+        return true;
     }
     public int setCurrencySpinner(){
         String str = pref.getString("currency","");
--- app/src/main/java/ca/uottawa/tipcalculator/Summary.java
+++ app/src/main/java/ca/uottawa/tipcalculator/Summary.java
@@ -5,12 +5,15 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 
-public class Summary extends AppCompatActivity {
+public class Summary extends AppCompatActivity implements View.OnClickListener {
     TextView bAmount, tAmount, tips, tipsPerPerson, eachPersonPays;
     SharedPreferences pref;
+    Button backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +37,16 @@ public class Summary extends AppCompatActivity {
         bAmount.setText(setCurrencySign() + amount);
         tipsPerPerson.setText(setCurrencySign() + tipsPP);
         eachPersonPays.setText(setCurrencySign() + eachPays);
+
+        backButton= (Button) findViewById(R.id.backButton);
+        backButton.setOnClickListener(this);
+    }
+
+    public void onClick(View v){
+        if(v.getId() == R.id.backButton){
+            Intent intent = new Intent(this,MainActivity.class);
+            startActivity(intent);
+        }
     }
 
     public String setCurrencySign() {
